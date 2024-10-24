1e1a338e519e0ce928a6d869a5af1ba1edcb307f
Gaini Laxman <gainilaxman12@gmail.com>
2024-10-24 16:08:48 +0530

    space removed

--- src/test/java/com/klinnovations/controllertest/StudentControllerTest.java
+++ src/test/java/com/klinnovations/controllertest/StudentControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import java.util.Arrays;
 import java.util.List;
 
