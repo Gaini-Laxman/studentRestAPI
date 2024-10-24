bf33813f249e501efec627266692a3878c87809e
Gaini Laxman <gainilaxman12@gmail.com>
2024-10-24 07:11:25 +0530

    second commit
    added unit testcases and sonar qube
    
    added unit test and mock added to controller and updated jacoco code covarage
    
    Modified controller test class

--- src/main/java/com/klinnovations/restcontroller/StudentController.java
+++ src/main/java/com/klinnovations/restcontroller/StudentController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,29 +26,29 @@ public class StudentController {
 
 	@PostMapping("/cteatestudent")
 	public ResponseEntity<Student> createStudent(@RequestBody Student student) {
-		Student createStudent = studentService.createStudent(student);
+		var createStudent = studentService.createStudent(student);
 		return new ResponseEntity<>(createStudent, HttpStatus.CREATED);
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Student> getStudentById(@PathVariable("id") Integer id) {
-		Student student = studentService.getStudentById(id);
-		return new ResponseEntity<Student>(student, HttpStatus.OK);
+		var student = studentService.getStudentById(id);
+		return new ResponseEntity<>(student, HttpStatus.OK);
 
 	}
 
 	@GetMapping("/allstudent")
 	public ResponseEntity<List<Student>> getAllStudents() {
 		List<Student> student = studentService.getAllStudents();
-		return new ResponseEntity<List<Student>>(student, HttpStatus.OK);
+		return new ResponseEntity<>(student, HttpStatus.OK);
 	}
 
 	@PutMapping("/{id}")
 
 	public ResponseEntity<Student> updateStudent(@PathVariable("id") Integer id, @RequestBody Student student) {
-		Student updateStudent = studentService.updateStudent(id, student);
+		var updateStudent = studentService.updateStudent(id, student);
 
-		return new ResponseEntity<Student>(updateStudent, HttpStatus.OK);
+		return new ResponseEntity<>(updateStudent, HttpStatus.OK);
 
 	}
 
@@ -57,7 +56,7 @@ public class StudentController {
 	public ResponseEntity<Student> deleteStudent(@PathVariable Integer id) {
 
 		studentService.deleteStudent(id);
-		return new ResponseEntity<Student>(HttpStatus.NO_CONTENT);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
 }
